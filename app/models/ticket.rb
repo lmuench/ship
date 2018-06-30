aa9560259caf507a9bb032107ffdc323936c0557
Ludwig Muench <muench.ludwig@gmail.com>
2018-06-30 18:50:34 +0200

    pre-define selectable attributes in config file

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,6 @@
 class Ticket < ApplicationRecord
   belongs_to :assignee, class_name: 'User'
   belongs_to :creator, class_name: 'User'
+
+  validates_inclusion_of :category, in: TicketChoices['category']
 end
---
+++ config/initializers/load_choices.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+TicketChoices = YAML.load_file("#{Rails.root}/config/choices/ticket.yml")
