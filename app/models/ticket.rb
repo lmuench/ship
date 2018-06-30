6a4d27ad26a47b2e8ca0805bed642315396082fe
Ludwig Muench <muench.ludwig@gmail.com>
2018-06-30 20:33:52 +0200

    add validations to ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,5 +4,9 @@ class Ticket < ApplicationRecord
   belongs_to :assignee, class_name: 'User'
   belongs_to :creator, class_name: 'User'
 
+  validates_presence_of :title
   validates_inclusion_of :category, in: TicketChoices['category']
+  validates_inclusion_of :priority, in: TicketChoices['priority']
+  validates_inclusion_of :status, in: TicketChoices['status']
+  validates_inclusion_of :assignee_id, in: User.all.map(&:id)
 end
